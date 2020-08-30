40aabb4e29d591a70d924a3fb7574f903a5336ae
jainashishr <arj000239@gmail.com>
2020-08-30 12:39:11 +0530

    Updates

---
+++ precache-manifest.9b6b40cc5297443d04b1e1b5468c308b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc63a9e9b698ade699441d32190c9e83",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a36ed9f29c5a68e200d6",
+    "url": "/monsters-rolodex/static/css/main.897f0ee9.chunk.css"
+  },
+  {
+    "revision": "61969e2b62afba81def7",
+    "url": "/monsters-rolodex/static/js/2.303b9bdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.303b9bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36ed9f29c5a68e200d6",
+    "url": "/monsters-rolodex/static/js/main.97017cb4.chunk.js"
+  },
+  {
+    "revision": "3c7880cff83e77693273",
+    "url": "/monsters-rolodex/static/js/runtime-main.79df5f2c.js"
+  }
+]);
